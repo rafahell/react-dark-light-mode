1d9c7aa8bf477a9a96b6b2506fc696b7bf413333
Rafael <220rrs@gmail.com>
2020-03-27 12:14:26 +0000

    prod files

---
+++ build/precache-manifest.ebaeb96d01f347e2d3bda8257e13735a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "387ff1c36c7d98e8ae73226502ff31b0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a6988ab614ef2b55279b",
+    "url": "./static/css/2.aeed1b96.chunk.css"
+  },
+  {
+    "revision": "4db99cf1fe1657f0adac",
+    "url": "./static/css/main.93b01f51.chunk.css"
+  },
+  {
+    "revision": "a6988ab614ef2b55279b",
+    "url": "./static/js/2.9b128a52.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "./static/js/2.9b128a52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4db99cf1fe1657f0adac",
+    "url": "./static/js/main.fd74926c.chunk.js"
+  },
+  {
+    "revision": "c6bd8383b87bc3619c3b",
+    "url": "./static/js/runtime-main.b4f23a64.js"
+  },
+  {
+    "revision": "c8bee5e8ea6856a796294bdf90c32d50",
+    "url": "./static/media/Daytime.c8bee5e8.svg"
+  },
+  {
+    "revision": "c874285c951a3aae4182cf21371a4e81",
+    "url": "./static/media/Nighttime.c874285c.svg"
+  },
+  {
+    "revision": "a0e02079b46eb6977f30b1de58601fce",
+    "url": "./static/media/StRyde-Medium.a0e02079.woff2"
+  },
+  {
+    "revision": "b4930a5fd2e38bad019082c77682316d",
+    "url": "./static/media/StRyde-Medium.b4930a5f.woff"
+  },
+  {
+    "revision": "b6f6528d11d4865450c45e20779b7eaf",
+    "url": "./static/media/StRyde-Regular.b6f6528d.woff2"
+  },
+  {
+    "revision": "f7d4c21fa82e0b14f2a6b168a79d8b0e",
+    "url": "./static/media/StRyde-Regular.f7d4c21f.woff"
+  }
+]);
